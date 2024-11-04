b748eddebc66079184a925b126c623cad461c482
tavares <140452343+spankaxota@users.noreply.github.com>
2024-11-04 08:13:10 -0300

    toma

---
+++ perguntas.js
@@ -0,0 +1,103 @@
+criaCartao(
+    'biologia',
+    'As células são compostas quimicamente por substâncias orgânicas, como carboidratos, lipídios e proteínas, e inorgânicas, como água e sais minerais.Os carboidratos, por exemplo, além de conferir energia às células, também se destacam pela função estruturaL:',
+    'Na parede celular'
+)
+criaCartao(
+    'biologia',
+    'Os componentes celulares que estão presentes tanto em células de eucariontes como de procariontes são:',
+    'membrana plasmática e ribossomos.'
+)
+criaCartao(
+    'biologia',
+    'I. Somos constituídos por células mais semelhantes às amebas do que às algas unicelulares.II. Meiose é um processo de divisão celular que só ocorre em células diploides.III. Procariontes possuem todas as organelas citoplasmáticas de um eucarionte, porém não apresentam núcleo.Está correto o que se afirma em:',
+    'I, apenas.'
+)
+criaCartao(
+    'biologia',
+    'Entre as características de uma célula vegetal, que a distingue de uma célula animal, podem ser citadas a presença de:',
+    'parede celular e a ocorrência da fotossíntese, processo que depende de água, gás carbônico e luz.'
+)
+criaCartao(
+    'biologia',
+    'Qual é a função dos ribossomos nas células?',
+    'Os ribossomos são responsáveis pela síntese de proteínas, traduzindo o RNA mensageiro em cadeias de aminoácidos.'
+)
+criaCartao(
+    'historia',
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'George Washington.'
+)
+criaCartao(
+    'historia',
+    'Em que ano ocorreu a independência do Brasil?',
+    '1822'
+)
+criaCartao(
+    'historia',
+    'Qual foi o evento que marcou o início da Revolução Francesa?',
+    'A Tomada da Bastilha, em 14 de julho de 1789.'
+)
+criaCartao(
+    'historia',
+    'Quem foi a rainha da Inglaterra durante a Era Elisabetana?',
+    'Rainha Elizabeth I.'
+)
+criaCartao(
+    'historia',
+    'O que foi a Guerra Fria?',
+    'Um período de tensão política e militar entre os Estados Unidos e a União Soviética, sem um confronto direto, que durou aproximadamente de 1947 a 1991.'
+)
+criaCartao(
+    'geografia',
+    'Qual é o maior oceano do mundo?',
+    'Oceano Pacífico.'
+)
+criaCartao(
+    'geografia',
+    'Qual é a capital do Brasil?',
+    'Brasília.'
+)
+criaCartao(
+    'geografia',
+    'Em qual continente fica o deserto do Saara?',
+    'África.'
+)
+criaCartao(
+    'geografia',
+    'Qual é o rio mais longo do mundo?',
+    'Rio Nilo.'
+)
+criaCartao(
+    'geografia',
+    'Qual país é conhecido como a "Terra do Sol Nascente"?',
+    'Japão.'
+)
+criaCartao(
+    'filosofia',
+    'Quem é considerado o pai da filosofia ocidental?',
+    'Sócrates.'
+)
+criaCartao(
+    'filosofia',
+    'Qual é a principal obra de Platão?',
+    'A República.'
+)
+criaCartao(
+    'filosofia',
+    'O que Aristóteles ensinou?',
+    'Aristóteles ensinou que a felicidade vem do equilíbrio.'
+    
+
+)
+criaCartao(
+    'filosofia',
+    'O que Platão acreditava?',
+    'Platão acreditava que existe um mundo perfeito de ideias que não podemos ver.'
+
+)
+criaCartao(
+    'filosofia',
+    'Quem foi Sócrates?',
+    'Sócrates foi um filósofo grego que ensinava por meio de perguntas para ajudar as pessoas a pensar melhor.'
+)
